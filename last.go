7c73682d797d718a14121c0abd27595afa49ff3b
wnjuguna wnjuguna <wm453856@gmail.com>
2024-05-23 16:44:25 +0300

    additional rev files

---
+++ last.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		return
+	}
+	arg := os.Args[1]
+	s := ""
+
+	for i := len(arg) - 1; i >= 0; i-- {
+		if arg[i] != ' ' {
+			s = string(arg[i]) + s
+		} else {
+			if s != "" {
+				break
+			}
+		}
+	}
+	for _, char := range s {
+		z01.PrintRune(char)
+	}
+	z01.PrintRune('\n')
+}
